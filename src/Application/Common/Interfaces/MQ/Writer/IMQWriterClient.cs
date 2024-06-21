05a02117190498c5eff9eb2f4f171f421b10141c
jitesh sonkusare <sonkusarejitesh@dnb.no>
2024-06-21 12:15:39 +0200

    ref

--- src/Application/Common/Interfaces/MQ/Writer/IMQWriterClient.cs
+++ src/Application/Common/Interfaces/MQ/Writer/IMQWriterClient.cs
@@ -1,5 +1,9 @@
-﻿namespace Application.Common.Interfaces.MQ;
+﻿using Shared.Wrapper;
+
+namespace Application.Common.Interfaces.MQ;
 
 public interface IMQWriterClient
 {
+	Task InitializeMQClientAsync(string queueManagerName, string queueName, IDictionary<string, object> mqProperties);
+	Task<Result> ExecuteWithRetryAsync();
 }
